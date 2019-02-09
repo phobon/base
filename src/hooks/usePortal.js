dd25c1d54469a476556c3e098548319cf0c14d58
Ben McCormick <exponen7@gmail.com>
2019-02-09 16:51:26 +0800

    Using state to handle portals... for now

--- src/hooks/usePortal.js
+++ src/hooks/usePortal.js
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import { createPortal } from 'react-dom';
 
 const selector = '#base__portal';
-let container = document.querySelector(selector);
 
 const usePortal = (child) => {
-if (!container) {
-    container = document.createElement('div');
-    container.setAttribute('id', selector);
-    container.style.cssText = 'left:24px;top:24px;right:24px;bottom:24px;position:fixed;pointer-events:none;display:flex;flex-direction:column;align-items:flex-end;justify-content:flex-end;';
-    document.querySelector('body').appendChild(container);
+  const [container, setContainer] = useState(null);
+
+  if (!container) {
+    const c = document.createElement('div');
+    c.setAttribute('id', selector);
+    c.style.cssText = 'left:24px;top:24px;right:24px;bottom:24px;position:fixed;pointer-events:none;display:flex;flex-direction:column;align-items:flex-end;justify-content:flex-end;';
+    document.querySelector('body').appendChild(c);
+
+    setContainer(c);
   }
 
   return createPortal(child, container);
