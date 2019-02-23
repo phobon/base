33cf3a6f6c5dad6f29e8a802dd47a4ecc8853c82
Ben McCormick <exponen7@gmail.com>
2019-02-23 15:53:26 +0800

    Optimising useTheme

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,21 +2,17 @@ import { useState, useEffect } from 'react';
 import { theme } from '../tokens';
 import { colour } from '../tokens/palettes';
 
-const getTheme = type => {
-  const themeDefinitions = {
-    light: {
-      foreground: colour.grayscale[0],
-      background: theme.colors.white,
-      grayscale: [...colour.grayscale],
-    },
-    dark: {
-      foreground: 'hsl(220, 4%, 98%)',
-      background: 'hsl(228, 34%, 9%)',
-      grayscale: [...colour.reverseGrayscale],
-    },
-  };
-
-  return themeDefinitions[type];
+const themeDefinitions = {
+  light: {
+    foreground: colour.grayscale[0],
+    background: theme.colors.white,
+    grayscale: [...colour.grayscale],
+  },
+  dark: {
+    foreground: 'hsl(220, 4%, 98%)',
+    background: 'hsl(228, 34%, 9%)',
+    grayscale: [...colour.reverseGrayscale],
+  },
 };
 
 const useTheme = () => {
@@ -24,7 +20,7 @@ const useTheme = () => {
 
   useEffect(() => void requestAnimationFrame(() => {
     const root = document.querySelector(':root');
-    const newTheme = getTheme(theme, root);
+    const newTheme = themeDefinitions[theme];
 
     root.style.setProperty('--c-foreground', newTheme.foreground);
     root.style.setProperty('--c-background', newTheme.background);
