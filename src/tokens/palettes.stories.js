e4b72f000585921dbe4f98f67fa4455bba83e114
Ben McCormick <exponen7@gmail.com>
2019-03-25 07:50:32 +0800

    Setting up some more palette tests

--- src/tokens/palettes.stories.js
+++ src/tokens/palettes.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { Box, Grid, Heading, Text } from '../components';
+import { Box, Flex, Grid, Heading, Text } from '../components';
 
 import { colour } from './palettes';
 
@@ -11,25 +11,49 @@ storiesOf('Tokens/Palettes', module)
     grayscale.reverse();
 
     return (
-    <Grid fullWidth fullHeight gridTemplateColumns="repeat(4, 1fr)" gridAutoRows="20rem" gridGap={8} bg="background" p={8}>
-      {grayscale.map(g => (
-        <Box fullWidth fullHeight borderRadius={4} bg={g}>
-          <Heading.H1 color="foreground">Text Here</Heading.H1>
-        </Box>
-      ))}
-    </Grid>
+      <Grid fullWidth fullHeight gridTemplateColumns="repeat(3, 1fr)" gridAutoRows="auto" gridGap={8} bg="background" p={8}>
+        {grayscale.map((g, i) => (
+          <Box fullWidth fullHeight key={g}>
+            <Box flexDirection="column" alignItems="flex-start" mr={3}>
+              <Heading.H1 color={g} mb={3}>Heading.H1</Heading.H1>
+              <Heading.H2 color={g} mb={3}>Heading.H2</Heading.H2>
+              <Heading.H3 color={g} mb={3}>Heading.H3</Heading.H3>
+              <Heading.H4 color={g} mb={3}>Heading.H4</Heading.H4>
+              <Heading.H5 color={g} mb={3}>Heading.H5</Heading.H5>
+              <Heading.H6 color={g} mb={3}>Heading.H6</Heading.H6>
+              <Text color={g} mb={5}>Some Text here</Text>
+            </Box>
+          
+            <Flex fullWidth fullHeight borderRadius={4} bg={g}>
+              <Heading.H1 color="foreground">Text Here</Heading.H1>
+            </Flex>
+          </Box>
+        ))}
+      </Grid>
   )})
   .add('With dark theme', () => {
     const grayscale = [...colour.reverseGrayscale];
     grayscale.reverse();
   
     return (
-    <Grid fullWidth fullHeight gridTemplateColumns="repeat(4, 1fr)" gridAutoRows="20rem" gridGap={8} bg="hsl(228, 22%, 22%)" p={8}>
-      {grayscale.map(g => (
-        <Box fullWidth fullHeight borderRadius={4} bg={g}>
-          <Heading.H1 color="hsl(220, 4%, 98%)">Text Here</Heading.H1>
-        </Box>
-      ))}
-    </Grid>
+      <Grid fullWidth gridTemplateColumns="repeat(3, 1fr)" gridAutoRows="auto" gridGap={8} bg="hsl(228, 22%, 22%)" p={8}>
+        {grayscale.map(g => (
+          <Box fullWidth fullHeight key={g}>
+            <Box flexDirection="column" alignItems="flex-start" mr={3}>
+              <Heading.H1 color={g} mb={3}>Heading.H1</Heading.H1>
+              <Heading.H2 color={g} mb={3}>Heading.H2</Heading.H2>
+              <Heading.H3 color={g} mb={3}>Heading.H3</Heading.H3>
+              <Heading.H4 color={g} mb={3}>Heading.H4</Heading.H4>
+              <Heading.H5 color={g} mb={3}>Heading.H5</Heading.H5>
+              <Heading.H6 color={g} mb={3}>Heading.H6</Heading.H6>
+              <Text color={g} mb={5}>Some Text here</Text>
+            </Box>
+           
+            <Flex fullWidth fullHeight borderRadius={4} bg={g}>
+              <Heading.H1 color="hsl(220, 4%, 98%)">Text Here</Heading.H1>
+            </Flex>
+          </Box>
+        ))}
+      </Grid>
   )});
 
