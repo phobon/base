32d8716d86959e7b5be7ea3f49e35146ae99367a
Ben McCormick <exponen7@gmail.com>
2019-03-26 16:42:20 +0800

    Fixing a bug with destructureLayoutProps

--- src/utils/destructureLayoutProps.js
+++ src/utils/destructureLayoutProps.js
@@ -1,11 +1,10 @@
 /**
  * A utility function to destructure layout props from a set of props.
  * This is useful for positioning items within an Higher Order Component and simplify the incoming API.
- * @param {} props 
  */
 const destructureLayoutProps = (props) => {
-  const { m, my, mx, mt, mb, ml, mr, position, top, left, right, bottom, fullWidth, fullHeight, ...passthroughProps } = props;
-  const layoutProps = { m, my, mx, mt, mb, ml, mr, top, left, right, bottom, fullWidth, fullHeight };
+  const { m, my, mx, mt, mb, ml, mr, position, top, left, right, bottom, width, height, fullWidth, fullHeight, ...passthroughProps } = props;
+  const layoutProps = { m, my, mx, mt, mb, ml, mr, top, left, right, bottom, fullWidth, fullHeight, width, height };
 
   return [layoutProps, passthroughProps];
 };
