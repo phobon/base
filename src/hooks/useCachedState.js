72f0daffd8d6668ddff4c7e29ba6ccef13b764d9
Ben McCormick <exponen7@gmail.com>
2019-04-02 20:29:23 +0800

    Fixing initialValue issues with useCachedState

--- src/hooks/useCachedState.js
+++ src/hooks/useCachedState.js
@@ -3,7 +3,19 @@ import { useState, useCallback } from 'react';
 const stateKey = 'base__cachedState';
 
 const useCachedState = (key, initialValue = null) => {
-  const [state, setState] = useState(window.localStorage.getItem(stateKey) || { key: initialValue });
+  const [state, setState] = useState(window.localStorage.getItem(stateKey));
+
+  // We need to get creative about how we set the initial state here.
+  // If there's no localStorage item and the initialValue is not null, set it.
+  // Pass in the key as a dep so this doesn't get called again til the key changes.
+  useEffect(() => {
+    if (!state && initialValue) {
+      const s = {};
+      s[key] = initialValue;
+      setState(state);
+      window.localStorage.setItem(stateKey, state);
+    };
+  }, [key])
 
   const setCachedState = useCallback((value) => {
     state[key] = value;
