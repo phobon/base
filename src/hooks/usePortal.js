3c648903f070a6dd529c2cbbb3f6ad534298c39d
Ben McCormick <exponen7@gmail.com>
2019-04-02 15:53:43 +0800

    Fixing an issue with usePortal with selectors

--- src/hooks/usePortal.js
+++ src/hooks/usePortal.js
@@ -12,8 +12,8 @@ const usePortal = (child, selector = '#base__portal') => {
     let c = document.querySelector(selector);
     if (!c) {
       c = document.createElement('div');
-      c.setAttribute('id', selector);
-      c.style.cssText = 'left:0;top:0;right:0;bottom:0;position:fixed;pointer-events:none;display:flex;flex-direction:column;align-items:flex-end;justify-content:flex-end;';
+      c.setAttribute('id', selector.startsWith('#') ? selector.slice(1) : selector);
+      c.style.cssText = 'left:0;top:0;right:0;bottom:0;position:fixed;pointer-events:none;display:flex;';
       document.querySelector('body').appendChild(c);
     }
     
