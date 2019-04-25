432d88570fc67f0ff6a35a94d518c2be2ece307a
Ben McCormick <exponen7@gmail.com>
2019-04-25 11:19:37 +0800

    Adding some more extensible properties to Label

--- src/components/form/Label.js
+++ src/components/form/Label.js
@@ -1,18 +1,21 @@
 import styled from 'styled-components';
 import Text from '../typography/Text';
+import { flexbox } from '../../utils';
 
 const Label = styled(Text).attrs(() => ({ as: 'label' }))({
-  display: 'flex',
-  flex: 'none',
-  alignItems: 'center',
-  justifyContent: 'center',
-});
+    display: 'flex',
+  },
+  flexbox,
+);
 
 Label.displayName = 'Label';
 
 Label.defaultProps = Object.assign({}, Text.defaultProps, {
   color: 'grayscale.2',
   fontSize: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  flex: 'none',
 });
 
 export default Label;
