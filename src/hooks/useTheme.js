554c45f06353d0b5773f819643b9a04ba3547c9f
phobon <exponen7@gmail.com>
2020-04-19 13:33:21 +0800

    Fixing useTheme for SSR

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -7,21 +7,24 @@ const useTheme = (initial = 'light', themeFunction = getTheme) => {
   const [theme, setTheme] = useCachedState('phobon__base:theme', initial);
 
   useEffect(() => {
-    if (theme) {
-      requestAnimationFrame(() => { // eslint-disable-line no-void
-        const root = document.querySelector(':root');
-        const newTheme = themeFunction(theme);
-    
-        root.style.setProperty('--c-foreground', newTheme.foreground);
-        root.style.setProperty('--c-background', newTheme.background);
-        newTheme.grayscale.forEach((c, i) => {
-          root.style.setProperty(`--c-grayscale-${i}`, c);
-        });
-      });
+    if (!theme) {
+      return;
     }
+
+    requestAnimationFrame(() => {
+      const root = document.querySelector(':root');
+      const newTheme = themeFunction(theme);
+  
+      root.style.setProperty('--c-foreground', newTheme.foreground);
+      root.style.setProperty('--c-background', newTheme.background);
+      newTheme.grayscale.forEach((c, i) => {
+        root.style.setProperty(`--c-grayscale-${i}`, c);
+      });
+    });
   }, [theme]);
 
   return [theme, setTheme];
 };
+
 
 export default useTheme;
